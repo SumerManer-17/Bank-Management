7348e16e901e23e564a41fbb9c5237064be627cc
Sumer <sumerinusmaner1711@gmail.com>
2026-07-28 16:28:18 +0530

    Added Account,Customer,Transaction classes

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,14 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        boolean running=true;
         System.out.println("Welcome to Console Bank");
-        System.out.println("""
+        while(running){
+            System.out.println("""
                 1)Open Account
                 2)Deposit
                 3)Withdraw
@@ -13,5 +18,57 @@ public class Main {
                 7)Search Account by Customer Name
                 0)Exit
                 """);
+
+            System.out.print("CHOOSE: ");
+            String choice=scanner.nextLine().trim();
+            System.out.println("Choice "+choice);
+
+
+            switch (choice){
+                case "1" -> openAccount(scanner);
+                case "2" -> deposit(scanner);
+                case "3" -> withdraw(scanner);
+                case "4" -> transfer(scanner);
+                case "5" -> statements(scanner);
+                case "6" -> listAccounts(scanner);
+                case "7" -> searchAccount(scanner);
+                case "0" -> running=false;
+            }
+        }
+    }
+
+    private static void searchAccount(Scanner scanner) {
+    }
+
+    private static void listAccounts(Scanner scanner) {
+        
+    }
+
+    private static void statements(Scanner scanner) {
+        
+    }
+
+    private static void transfer(Scanner scanner) {
+        
+    }
+
+    private static void withdraw(Scanner scanner) {
+        
+    }
+
+    private static void deposit(Scanner scanner) {
+        
+    }
+
+    private static void openAccount(Scanner scanner) {
+        System.out.println("Customer Name: ");
+        String name=scanner.nextLine().trim();
+        System.out.println("Customer Email: ");
+        String email=scanner.nextLine().trim();
+        System.out.println("Account Type (SAVING/CURRENT): ");
+        String type=scanner.nextLine().trim();
+        System.out.println("Initial Deposite (OPTIONAL): ");
+        String amountStr=scanner.nextLine().trim();
+        Double initial=Double.valueOf(amountStr);
     }
 }
